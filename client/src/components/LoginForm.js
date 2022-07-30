8f2957a1252bd9f0678a92cdc121c3cbc52f3cf7
Dimos <dimospagkas@yahoo.gr>
2022-07-30 12:36:22 -0400

    update code to reduce empty lines

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,90 +1,85 @@
 // see SignupForm.js for comments
-import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import React, { useState } from "react"
+import { Alert, Button, Form } from "react-bootstrap"
 
 // import { loginUser } from '../utils/API';
-import Auth from '../utils/auth';
+import Auth from "../utils/auth"
 
 // importing mutations
-import { useMutation } from '@apollo/react-hooks';
-import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from "@apollo/react-hooks"
+import { LOGIN_USER } from "../utils/mutations"
 
 const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-  const [validated] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
+    const [userFormData, setUserFormData] = useState({ email: "", password: "" })
+    const [validated] = useState(false)
+    const [showAlert, setShowAlert] = useState(false)
 
-  // initialize mutation
-  const [loginUser, {error} ] = useMutation(LOGIN_USER);
+    // initialize mutation
+    const [loginUser, { error }] = useMutation(LOGIN_USER)
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
-  };
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const { data } = await loginUser({
-        variables: {...userFormData} 
-      });
-      Auth.login(data.login.token);
-    } catch (e) {
-      console.error(e);
-      setShowAlert(true);
+    const handleInputChange = event => {
+        const { name, value } = event.target
+        setUserFormData({ ...userFormData, [name]: value })
     }
 
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
-  };
+    const handleFormSubmit = async event => {
+        event.preventDefault()
 
-  return (
-    <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your login credentials!
-        </Alert>
-        <Form.Group>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your email'
-            name='email'
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
+        try {
+            const { data } = await loginUser({
+                variables: { ...userFormData },
+            })
+            Auth.login(data.login.token)
+        } catch (e) {
+            console.error(e)
+            setShowAlert(true)
+        }
 
-        <Form.Group>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={!(userFormData.email && userFormData.password)}
-          type='submit'
-          variant='success'>
-          Submit
-        </Button>
-        {error && <div>Login failed</div>}
-      </Form>
-    </>
-  );
-};
+        setUserFormData({
+            username: "",
+            email: "",
+            password: "",
+        })
+    }
 
-export default LoginForm;
+    return (
+        <>
+            <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+                <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant="danger">
+                    Something went wrong with your login credentials!
+                </Alert>
+                <Form.Group>
+                    <Form.Label htmlFor="email">Email</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Your email"
+                        name="email"
+                        onChange={handleInputChange}
+                        value={userFormData.email}
+                        required
+                    />
+                    <Form.Control.Feedback type="invalid">Email is required!</Form.Control.Feedback>
+                </Form.Group>
 
+                <Form.Group>
+                    <Form.Label htmlFor="password">Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Your password"
+                        name="password"
+                        onChange={handleInputChange}
+                        value={userFormData.password}
+                        required
+                    />
+                    <Form.Control.Feedback type="invalid">Password is required!</Form.Control.Feedback>
+                </Form.Group>
+                <Button disabled={!(userFormData.email && userFormData.password)} type="submit" variant="success">
+                    Submit
+                </Button>
+                {error && <div>Login failed</div>}
+            </Form>
+        </>
+    )
+}
 
+export default LoginForm
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,10 +1,10 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag"
 
 // mutation for logged in user
 export const LOGIN_USER = gql`
     mutation loginUser($email: String!, $password: String!) {
         login(email: $email, password: $password) {
-        token
+            token
             user {
                 _id
                 username
@@ -17,17 +17,17 @@ export const LOGIN_USER = gql`
                     authors
                     link
                     image
-         }
-       }
-     }
-   }
-`;
+                }
+            }
+        }
+    }
+`
 
-// mutation to add user 
+// mutation to add user
 export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
-        token
+            token
             user {
                 _id
                 username
@@ -44,7 +44,7 @@ export const ADD_USER = gql`
             }
         }
     }
-`;
+`
 
 // mutation to save books
 export const SAVE_BOOK = gql`
@@ -63,7 +63,7 @@ export const SAVE_BOOK = gql`
             }
         }
     }
-`;
+`
 
 // mutation to remove books
 export const REMOVE_BOOK = gql`
@@ -83,4 +83,4 @@ export const REMOVE_BOOK = gql`
             }
         }
     }
-`;
+`
